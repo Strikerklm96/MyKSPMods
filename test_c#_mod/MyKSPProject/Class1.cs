a2bd6b9b7b157f14414aeeecf7696445d9c9d234
Leon <Strikerklm96@gmail.com>
2018-02-17 12:16:47 -0600

    KSP C# Mod;

---
+++ test_c#_mod/MyKSPProject/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace MyKSPProject
+{
+    /// <summary>
+    /// My first part!
+    /// </summary>
+    public class HelloKerbinMod : PartModule
+    {
+        /// <summary>
+        /// Called when the part is started by Unity.
+        /// </summary>
+        public override void OnStart(StartState state)
+        {
+            // Add stuff to the log
+            print("Hello, Kerbin!");
+        }
+    }
+}
